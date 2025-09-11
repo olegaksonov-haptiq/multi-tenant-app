f671311ddc89bd2f8c0ae0aaeb709aedae7532b2
vishwajit-haptiq <Vishwajit.Khairnar@haptiq.com>
2025-09-11 21:38:39 +0530

    added common help function for getCookie

---
+++ src/utils/common.tsx
@@ -0,0 +1,7 @@
+/**
+ * Helper to get a cookie value by name
+ */
+export const getCookie = (name: string): string | null => {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? decodeURIComponent(match[2]) : null;
+};
